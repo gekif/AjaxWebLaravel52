333211e479c8551efd3da51d279b913a3bd9b593
Gekif <dzulfikar.maulana@gmail.com>
2020-03-04 20:07:32 +0700

    02 Project 1 - Build Contact Manager Application: 008 Create Application Layout (Fixing)

--- contact_manager/resources/views/contacts/index.blade.php
+++ contact_manager/resources/views/contacts/index.blade.php
@@ -2,10 +2,9 @@
 
 @section('content')
 
-<div class="panel panel-default">
+  <div class="card">
+    <div class="card-header"><strong>All Contacts</strong></div>
     <table class="table">
-
-    @foreach ($contacts as $contact)
       <tr>
         <td class="middle">
           <div class="media">
@@ -15,51 +14,71 @@
               </a>
             </div>
             <div class="media-body">
-              <h4 class="media-heading">{{ $contact->name }}</h4>
+              <h4 class="media-heading">Contact 1</h4>
               <address>
-                <strong>{{ $contact->company }}</strong><br>
-                {{ $contact->email }}
+                <strong>Job</strong><br>
+                contact1@sample.com
               </address>
             </div>
           </div>
         </td>
         <td width="100" class="middle">
           <div>
-            <a href="#" class="btn btn-circle btn-default btn-xs" title="Edit">
-              <i class="glyphicon glyphicon-edit"></i>
+            <a href="#" class="btn btn-outline-secondary btn-circle btn-xs" title="Edit">
+              <i class="fa fa-edit"></i>
             </a>
-            <a href="#" class="btn btn-circle btn-danger btn-xs" title="Edit">
-              <i class="glyphicon glyphicon-remove"></i>
+            <a href="#" class="btn btn-outline-danger btn-circle btn-xs" title="Delete">
+              <i class="fa fa-times"></i>
             </a>
           </div>
         </td>
       </tr>
-
-    @endforeach
-      
-    </table>            
-  </div>
-
-  <div class="text-center">
-    <nav>
-      <ul class="pagination">
-        <li>
-          <a href="#" aria-label="Previous">
-            <span aria-hidden="true">&laquo;</span>
-          </a>
-        </li>
-        <li class="active"><a href="#">1</a></li>
-        <li><a href="#">2</a></li>
-        <li><a href="#">3</a></li>
-        <li><a href="#">4</a></li>
-        <li><a href="#">5</a></li>
-        <li>
-          <a href="#" aria-label="Next">
-            <span aria-hidden="true">&raquo;</span>
-          </a>
-        </li>
-      </ul>
-    </nav>
+      <tr>
+        <td class="middle">
+          <div class="media">
+            <div class="media-left">
+              <a href="#">
+                <img class="media-object" src="http://placehold.it/100x100" alt="...">
+              </a>
+            </div>
+            <div class="media-body">
+              <h4 class="media-heading">Contact 2</h4>
+              <address>
+                <strong>Job 2</strong><br>
+                contact2@sample.com
+              </address>
+            </div>
+          </div>
+        </td>
+        <td width="100" class="middle">
+          <div>
+            <a href="#" class="btn btn-outline-secondary btn-circle btn-xs" title="Edit">
+              <i class="fa fa-edit"></i>
+            </a>
+            <a href="#" class="btn btn-outline-danger btn-circle btn-xs" title="Edit">
+              <i class="fa fa-times"></i>
+            </a>
+          </div>
+        </td>
+      </tr>
+    </table>
+    <div class="card-footer">
+      <nav aria-label="Page Navigation">
+        <ul class="pagination justify-content-center">
+          <li class="page-item">
+            <a class="page-link" href="#">Previous</a>
+          </li>
+          <li class="page-item"><a class="page-link" href="#">1</a></li>
+          <li class="page-item active">
+            <a class="page-link" href="#">2</a>
+          </li>
+          <li class="page-item"><a class="page-link" href="#">3</a></li>
+          <li class="page-item">
+            <a class="page-link" href="#">Next</a>
+          </li>
+        </ul>
+      </nav>
+    </div>
   </div>
 
 @endsection
--- contact_manager/resources/views/layouts/main.blade.php
+++ contact_manager/resources/views/layouts/main.blade.php
@@ -1,73 +1,59 @@
-
 <!DOCTYPE html>
 <html lang="en">
-  <head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
-    <title>My Contact</title>
+<head>
+  <!-- Required meta tags -->
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
-    <!-- Bootstrap -->
-    <link href="/assets/css/bootstrap.min.css" rel="stylesheet">
-    <link href="/assets/css/custom.css" rel="stylesheet">
+  <title>My Contact</title>
 
-    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
-    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
-    <!--[if lt IE 9]>
-      <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
-      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
-    <![endif]-->
-  </head>
-  <body>
-    <!-- navbar -->
-    <nav class="navbar navbar-default navbar-fixed-top">
-      <div class="container">
-        <div class="navbar-header">
-          <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar-collapse" aria-expanded="false">
-            <span class="sr-only">Toggle navigation</span>
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
-          </button>
+  <!-- Bootstrap -->
+  <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet">
+  <link href="assets/css/bootstrap.min.css" rel="stylesheet">
+  <link href="assets/css/custom.css" rel="stylesheet">
+</head>
+<body>
+<!-- navbar -->
+<nav class="navbar navbar-expand-lg navbar-light">
+  <div class="container">
+    <a class="navbar-brand text-uppercase" href="index.html">
+      My contact
+    </a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo01" aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
 
-          <a class="navbar-brand text-uppercase" href="#">            
-            My contact
-          </a>
+    <!-- /.navbar-header -->
+    <div class="collapse navbar-collapse" id="navbarTogglerDemo02">
+      <ul class="navbar-nav ml-auto">
+        <li class="nav-item"><a href="form.html" class="btn btn-outline-primary">Add New</a></li>
+      </ul>
+    </div>
+  </div>
+</nav>
+
+<!-- content -->
+<main class="pt-5">
+  <div class="container">
+    <div class="row">
+      <div class="col-md-3">
+        <div class="list-group">
+          <a href="#" class="list-group-item list-group-item-action d-flex justify-content-between align-items-center active">All Contact <span class="badge badge-warning badge-pill">14</span></a>
+          <a href="#" class="list-group-item list-group-item-action d-flex justify-content-between align-items-center">Family <span class="badge badge-pill badge-warning">4</span></a>
+          <a href="#" class="list-group-item list-group-item-action d-flex justify-content-between align-items-center">Friends <span class="badge badge-pill badge-warning">3</span></a>
+          <a href="#" class="list-group-item list-group-item-action d-flex justify-content-between align-items-center">Other <span class="badge badge-pill badge-warning">3</span></a>
         </div>
-        <!-- /.navbar-header -->
-        <div class="collapse navbar-collapse" id="navbar-collapse">
-          <div class="nav navbar-right navbar-btn">
-            <a href="form.html" class="btn btn-default">
-              <i class="glyphicon glyphicon-plus"></i> 
-              Add Contact
-            </a>
-          </div>
-        </div>
-      </div>
-    </nav>
+      </div><!-- /.col-md-3 -->
 
-    <!-- content -->
-    <div class="container">
-      <div class="row">
-        <div class="col-md-3">
-          <div class="list-group">
-            <a href="#" class="list-group-item active">All Contact <span class="badge">10</span></a>
-            <a href="" class="list-group-item">Family <span class="badge">4</span></a>
-            <a href="" class="list-group-item">Friends <span class="badge">3</span></a>
-            <a href="" class="list-group-item">Other <span class="badge">3</span></a>
-          </div>
-        </div><!-- /.col-md-3 -->
-
-        <div class="col-md-9">
+      <div class="col-md-9">
           @yield('content')
-        </div>
       </div>
     </div>
+  </div>
+</main>
 
-    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
-    <script src="/assets/js/jquery.min.js"></script>
-    <!-- Include all compiled plugins (below), or include individual files as needed -->
-    <script src="/assets/js/bootstrap.min.js"></script>
-  </body>
+<script src="assets/js/jquery.min.js"></script>
+<script src="assets/js/popper.min.js"></script>
+<script src="assets/js/bootstrap.min.js"></script>
+</body>
 </html>
