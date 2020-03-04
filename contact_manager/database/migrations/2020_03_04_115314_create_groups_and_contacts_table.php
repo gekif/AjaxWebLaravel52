a9076a9274b76884d81106b0fe6924b42077af57
Gekif <dzulfikar.maulana@gmail.com>
2020-03-04 19:06:43 +0700

    02 Project 1 - Build Contact Manager Application: 006 Database Migration

---
+++ contact_manager/database/migrations/2020_03_04_115314_create_groups_and_contacts_table.php
@@ -0,0 +1,47 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateGroupsAndContactsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('groups', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name');
+            $table->timestamps();
+        });
+
+        Schema::create('contacts', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('group_id')->unsigned()->default(0);
+            $table->string('name');
+            $table->string('company');
+            $table->string('email');
+            $table->string('phone');
+            $table->string('address');
+            $table->timestamps();
+
+            $table->foreign('group_id')->references('id')
+                ->on('groups')
+                ->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('contacts');
+        Schema::drop('groups');
+    }
+}
